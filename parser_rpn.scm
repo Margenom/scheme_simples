2d6d2c391b19daefc254aaf3946513c509a1fa66
Margenom <55763275+Margenom@users.noreply.github.com>
2022-01-08 02:44:48 +0300

    Reverse Polish Notation parser

---
+++ parser_rpn.scm
@@ -0,0 +1,10 @@
+(import srfi-1 (chicken irregex) (chicken string))
+
+(define (parser expr) (car
+	(foldl (lambda(v c)
+		(if (or (number? c) (irregex-match "[a-z]" (symbol->string c)))
+			(append v (list c)) 
+			(append (take v (- (length v) 2)) (list (cons c (drop v (- (length v) 2))))))) 
+		'() (map
+			(lambda(k) ((if (irregex-match "[0-9]+" k) string->number string->symbol) k))
+			(string-split expr " ")))))
