6e6c4a8954f8766aab9c3b193df11783ba8008b9
Даниил Швачкин <margenom@yahoo.com>
2022-09-17 23:24:21 +0300

    PD

--- hash-table.scm
+++ hash-table.scm
@@ -1,7 +1,7 @@
 
-(define (hasher String Size)
+(define*(hasher String Size (Base 64))
 	(let ((Out 0)) (map (lambda(K) 
-		(set! Out (modulo (+ Out (char->integer K)) Size))) String) Out))
+		(set! Out (modulo (+ (* Base Out) (char->integer K)) Size))) String) Out))
 
 ; From chicken srfi-1
 (define (filter pred lis)     ; Sleazing with EQ? makes this 
@@ -17,10 +17,24 @@
 			(recur tail))))))     
 ; this one can be a tail call.  filter
 
+; Hash-table with list
 (define (ht-make Size) (make-vector Size #f))
-(define (ht-set! HT Key Val) 
+(define (ht-set! HT Key Val)
 	(let*((Hkey (hasher Key (length HT))) (Pair (list Key Val)) (Tval (HT Hkey)))
 		(vector-set! HT Hkey (if Val (if Tval (cons Pair Tval) (list Pair))
 			(filter (lambda(K) (not (string=? (car K) Key))) Tval))))) ; delete
 (define (ht-get HT Key) (let*((Hkey (hasher Key (length HT))) (Pairs (HT Hkey))) 
 	(if Pairs (cadr (assoc Key Pairs)) #f)))
+
+; Open adress hash-table
+(define htoa-make ht-make)
+(define (_htoa-get-index HTA Key Size) (let* (
+		(Hkey (hasher Key Size))
+		(Exst (and (HTA Hkey) (string=? Key (car (HTA Hkey)))))
+		(Hdis (or Exst (hasher Key (- Size 3))))
+		(Nind (and Hdis (let rec((Di Hdis)) (let ((P (HTA (modulo (+ Hkey Di) Size)))) (if (and P (not (string=? (car P) Key))) (rec (* Di Hdis)) Di))))))
+	(or Nind Hkey)))
+(define (htoa-set! HTA Key Val)
+	(vector-set! HTA (_htoa-get-index HTA Key (length HTA)) (cons Key Val)))
+(define (htoa-get HTA Key) (cdr (HTA (_htoa-get-index HTA Key (length HTA)))))
+(define (htoa-del HTA Key) (htoa-set! HTA Key #f)) 
--- to-bin.scm
+++ to-bin.scm
@@ -4,8 +4,9 @@
     (unless (or (zero? L) (zero? F)) (float-to-bin T (- L 1)))))
 
 (define (digit-to-bin D L)
+	(define (oneVzero V) (display (if (zero? V) 0 1)))
 	(let* ((Q (/ D 2)) (B (modulo Q 2)) (T (- Q B)))
-		(unless (or (zero? L) (= Q B)) (digit-to-bin T (- L 1)))
-		(display (if (zero? B) 0 1))))
+		(unless (or (zero? L)) (digit-to-bin T (- L 1)))
+		(oneVzero B)))
 
 (define (to-bin N L) (digit-to-bin (truncate N) L) (display ".") (float-to-bin (- N (truncate N)) L))
